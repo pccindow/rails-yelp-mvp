460864abdc7d4b3b255f17cebd7ce085fe00cfc8
Pierre-Christophe Callac <pccindo78@msn.com>
2017-02-15 12:18:14 +0100

    add restaurants attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    description:  "Face au jardin, on découvre une salle lumineuse... et la cuisine d'Éric Frechon.",
+    stars:        3
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    description:  "Une valeur sûre que cette belle maison du 17e et les recettes de Jean-Christophe Rizet",
+    stars:        1
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    description:  "Oeil vif, geste sûr: impossible de distinguer, dans les créations de Frédéric Anton...",
+    stars:        3
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
